02ac83eb58f16d96a2a0ef10dbaf772846c99a12
friketrike <friketrike@gmail.com>
2016-09-22 15:12:59 -0400

    up to question 1i

--- A1.m
+++ A1.m
@@ -13,9 +13,8 @@ function [] = A1  % Q 1a
     % plot x against y, use circles and avoid plotting connecting lines
     figure(1);
     plot(x, y, 'o');
-    title(['Comp 6321, Fall 2016 - A1 - Federico O''Reilly Regueiro -', ... 
-           ' data plot and regressions d = [1:3]']);
-    ylabel('y');
+    title(['data plot and regressions d = [1:3]']);
+    ylabel('y / hw(x)');
     xlabel('x');
 
     % Q 1b
@@ -53,36 +52,93 @@ function [] = A1  % Q 1a
     % Q 1h
     [d, train_error, test_error] = k_fold_cv (x, y, 5);
     figure(2);
-    plot(test_error);
+    plot(mean(test_error, 2));
     hold on;
-    plot(2:d+1,diff(test_error), 'r.');
-    test_error
-    diff(test_error)
+    diff_abscissa = 2:size(test_error,1);
+    mean_test_error = mean(test_error,2)
+    test_err_diff = diff(mean(test_error,2))
+    % plot non-positive finite difference
+    idx = test_err_diff < 0;
+    plot(diff_abscissa(idx),test_err_diff(idx), 'g*' );
+    % plot positive finite difference
+    idx = test_err_diff >= 0;
+    plot(diff_abscissa(idx),test_err_diff(idx), 'rx' );
+    plot(1:d+2, zeros(d+2, 1), 'k-');
     hold off;
-    title(['Comp 6321, Fall 2016 - A1 - Federico O''Reilly Regueiro -', ... 
-           ' data plot and regressions d = [1:3]']);
+    title(['Data plot and regressions d = [1:', num2str(d+1), ']']);
     ylabel('validation MSE');
     xlabel('d - order of polynomial regression');
-
+    legend('mean of testing errors (mte)', ...
+           'negative fd(mte) ( ie. mte(d) - mte(d-1) < 0 )', 
+           'positive fd(mte)');
     [wd, x_prime] = PolyRegress(x,y,d);
+    jh_d = trainingErr(x_prime, wd, y);
     figure(3);
     plot(x(:,1), y, 'o');
     hold on;
     plot(x(:, size(x,2)-1), x_prime*wd, 'r*');, 
     hold off;
+    title(['Data plot and polynomial regresion of order ', num2str(d), ...
+          ' obtained via 5-fold cross validation']);
+    ylabel('y / hw(x)');
+    xlabel('x');
+    legend('data points', [num2str(d), '-order polynomial regresion, MSE=', ...
+            num2str(jh_d)]);
+            
+            
+            
+            
+    % Q 1i
+    [d, train_error, test_error] = k_fold_cv (x, y, 5, true);
+    figure(4);
+    plot(mean(test_error, 2));
+    hold on;
+    diff_abscissa = 2:size(test_error,1);
+    mean_test_error = mean(test_error,2)
+    test_err_diff = diff(mean(test_error,2))
+    % plot non-positive finite difference
+    idx = test_err_diff < 0;
+    plot(diff_abscissa(idx),test_err_diff(idx), 'g*' );
+    % plot positive finite difference
+    idx = test_err_diff >= 0;
+    plot(diff_abscissa(idx),test_err_diff(idx), 'rx' );
+    plot(1:d+2, zeros(d+2, 1), 'k-');
+    hold off;
+    title(['Data plot and regressions d = [1:', num2str(d+1), ']']);
+    ylabel('validation MSE');
+    xlabel('d - order of polynomial regression');
+    legend('mean of testing errors (mte)', ...
+           'negative fd(mte) ( ie. mte(d) - mte(d-1) < 0 )', 
+           'positive fd(mte)');
+    [wd, x_prime] = PolyRegress(x,y,d, true);
+    jh_d = trainingErr(x_prime, wd, y);
+    figure(5);
+    plot(x(:,1), y, 'o');
+    hold on;
+    plot(x(:, size(x,2)-1), x_prime*wd, 'r*');, 
+    hold off;
+    title(['Data plot and polynomial regresion of order ', num2str(d), ...
+          ' obtained via 5-fold cross validation']);
+    ylabel('y / hw(x)');
+    xlabel('x');
+    legend('data points', [num2str(d), '-order polynomial regresion, MSE=', ...
+            num2str(jh_d)]);
 end
 
 % Q 1c
 function jh = trainingErr(x, w, y)
   hx = x*w;
-  % mean squared error? slides have the sum of squares but talk about mse...
+  % use mse, doesn't really matter but it's simpler to think of than total se
   jh = sum((hx - y).^2)/(2*size(x,1));
 end
 
 % Q 1d
-function [w, x_prime] = PolyRegress(x,y,d)
+function [w, x_prime] = PolyRegress(x, y, d, normalize)
+  if nargin < 4
+    normalize = false;
+  end
   if(d >= 1)
-    x_prime = format_poly(x,d);
+    x_prime = format_poly(x, d, normalize);
   else
     error('Error, d must be larger or equal to 1');
   end
@@ -90,8 +146,14 @@ function [w, x_prime] = PolyRegress(x,y,d)
 end
 
 % helper for PolyRegress
-function x_prime = format_poly(x,d) % TODO continue here and propagate normalization
-    x_prime = repmat(x(:,1), 1, d+1).^(repmat((d:-1:0), size(x,1), 1));
+function x_prime = format_poly(x,d, normalize) 
+  if nargin < 3
+    normalize = false;
+  end  
+  x_prime = repmat(x(:,1), 1, d+1).^(repmat((d:-1:0), size(x,1), 1));
+  if normalize
+  x_prime = x_prime * inv(diag(max(x_prime)));
+  end
 end
 
 function [d, train_error, test_error] = k_fold_cv (x, y, k, normalize)
@@ -99,26 +161,27 @@ function [d, train_error, test_error] = k_fold_cv (x, y, k, normalize)
     if mod(size(x,1), k)
         error('Matrix x must contain a multiple of k entries');
     end
-    if nargin < 3
+    if nargin < 4
         normalize = false;
     end
+    normalize
     m = size(x,1);
     % x_shuffle = x(randperm(m),:);
     x_shuffle = 1:m;
     
     % matlab abonimation for a do-while loop
     test_err_decreasing = true;
-    % first entry in the errors vector is the maximum possible fp num
+    % first two entries in the errors are the maximum possible fp num
     % just for the logic in the while loop (no do-while in matlab)
     % get rid of these two entries at the end
-    test_error = realmax;
-    train_error = realmax;
+    test_error = repmat(realmax, 2, k);
+    train_error = repmat(realmax, 2, k);
     d = 0;
-    while (d<8)%(test_err_decreasing)
+    while (test_err_decreasing)
         % create a blank entry to accumulate errors each fold
         d = d+1; 
-        test_error = [test_error; 0];
-        train_error = [train_error; 0];
+        %test_error = [test_error; 0];
+        %train_error = [train_error; 0];
         m_k = m/k;
         for i = 1:k
            idx_btm = 1:(m_k*(i-1));
@@ -126,32 +189,36 @@ function [d, train_error, test_error] = k_fold_cv (x, y, k, normalize)
            idx_top = (m_k*i)+1:m;
            x_train = [x(idx_btm,:);x(idx_top,:)];
            y_train = [y(idx_btm,:);y(idx_top,:)];
-           x_test = format_poly(x(idx_tst, :), d);
+           x_test = format_poly(x(idx_tst, :), d, normalize);
            y_test = y(idx_tst, :);
 
            % training
            %w = (x_train'*x_train) \ x_train'*y_train;
-           [w, x_train_prime] = PolyRegress(x_train,y_train,d);
+           [w, x_train_prime] = PolyRegress(x_train,y_train,d, normalize);
            jh_train = trainingErr(x_train_prime, w, y_train);
            
-           train_error(d+1) = train_error(d+1) + jh_train;
+           train_error(d+2, k) = jh_train;
            % validation
            jh_test = trainingErr(x_test, w, y_test);
-           test_error(d+1) = test_error(d+1) + jh_test;
+           test_error(d+2, k) = jh_test;
         end
         % TODO should our test allow one isolated small increase?
         % TODO also, change condition above used now to 'hack' d
-        test_err_decreasing = test_error(d+1) < test_error(d);  
+        test_err_decreasing = (mean(test_error(d+2,:))...
+             <= mean(test_error(d+1,:)))...
+                  || (mean(test_error(d+2,:)) - mean(test_error(d+1,:))) ...
+                  < 32 * abs(mean(test_error(d+1,:)) - mean(test_error(d,:)));
     end
     % TODO change the format of returned errors so we can get a variance
     % remove first entry for errors vectors
-    test_error(1,:) = [];
-    train_error(1,:) = [];
+    test_error(1:2,:) = [];
+    train_error(1:2,:) = [];
     % divide errors between k
     test_error = test_error/k;
     train_error = train_error/k;
     % d already stopped decreasing, optimal d has been passed
-    d = d-1;
+    
+    [dummy, d] = min(mean(test_error,2));
 end
 
   
